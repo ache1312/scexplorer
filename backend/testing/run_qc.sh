579078e12a6727a1097a43f2797e696bfb124e2c
ache1312 <felipehg1991@gmail.com>
2025-09-16 15:01:43 -0300

    remove test_files

--- backend/testing/run_qc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-python run_qc.py "$1" "$2" "$3" "$4" "$5"
--- backend/testing/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=qc_plots
-#SBATCH --output=test.out
-#SBATCH --error=test.err
-
-touch test_file
-echo "Testing directory access" > test_output.txt
